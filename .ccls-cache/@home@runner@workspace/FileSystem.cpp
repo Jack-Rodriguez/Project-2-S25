14cc47de6d649b9c1a869f0fa6713ae4e4583b17
Jack-Rodriguez <jaxrod02@gmail.com>
2025-03-08 18:48:19 +0000

    Completed the mkdir, touch, and ls functions

--- .ccls-cache/@home@runner@workspace/FileSystem.cpp
+++ .ccls-cache/@home@runner@workspace/FileSystem.cpp
@@ -1,6 +1,7 @@
 #include "FileSystem.hpp"
 #include <stdexcept>
 #include <sstream>
+using namespace std;
 
 FileSystemNode::FileSystemNode(std::string name, bool isDir) 
     : name(name), isDirectory(isDir), parent(nullptr) {}
@@ -20,18 +21,73 @@ FileSystem::~FileSystem() {
     delete root;
 }
 
-void FileSystem::mkdir(const std::string& name) {
-    //to do..
+//this function will make a new directory in the current directory
+void FileSystem::mkdir(const std::string& name) 
+{
+    //this loop will check if the directory name already exists in the current directory.
+    for(const auto& child : currentDirectory->children) // i learned this sytax from ChatGPT. I asked it for an unrelated example so that i didnt just copy it without understanding it.
+    {
+        //if the new name is the same as the name of the child, then it will throw an error.
+        if(child->name == name)
+        {
+            throw runtime_error("Directory already exists");
+        }
+    }
+
+    //makes a new directory with the given name, and sets it to being true (a directory)
+    FileSystemNode* newDir = new FileSystemNode(name, true);
+
+    //sets the new directory to point to the current directory as a parent
+    newDir->parent = currentDirectory;
+
+    //adds the new directory to the vector of children in the current directory
+    currentDirectory->children.push_back(newDir);
+
 }
 
-void FileSystem::touch(const std::string& name) {
-   //to do..
+//this function will make a new file in the current directory
+void FileSystem::touch(const std::string& name) 
+{
+    //this loop will check if the file name already exists in the current directory.
+   for(const auto& child : currentDirectory->children)
+    {
+        //if the new name is the same as the name of the child, then it will throw an error.
+        if(child->name == name)
+        {
+            throw runtime_error("File already exists");
+        }  
+    }
+    
+    //creates new file with the current name. Its a file because isDir is false
+    FileSystemNode* newFile = new FileSystemNode(name, false);
+
+    //sets the file to point to the current directory as a parent
+    newFile->parent = currentDirectory;
+
+    //adds the new file to the back of the children vector of the current directory
+    currentDirectory->children.push_back(newFile);
 }
 
-std::string FileSystem::ls() {
-    // to do..
-    // You can sstream here (like displayTree())
-    return 0;
+std::string FileSystem::ls() 
+{
+    //initializes streamed string
+    stringstream ss;
+
+    //for every child in the current directory, it will add the name to the stringstream
+    for(const auto& child : currentDirectory->children)
+    {
+        //if is is a directory, a slash is added to the end of the name
+        if(child->isDirectory)
+        {
+            ss << child->name << "/\n";
+        }
+        else{
+            ss << child->name << "\n";
+        }
+    }
+
+    //returns the stringstream as a string
+    return ss.str();
 }
 
 void FileSystem::cd(const std::string& path) {
